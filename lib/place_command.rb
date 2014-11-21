7a4a786fe0513053ba2cc0984f0cb9134b3ad0c9
getsometoast <hijameslewis@gmail.com>
2014-11-21 16:22:58 +1100

    add code to make test pass

--- lib/place_command.rb
+++ lib/place_command.rb
@@ -2,9 +2,10 @@ class PlaceCommand
   def initialize(**args)
     @robot = args[:robot]
     @position = args[:position]
+    @table = args[:table]
   end
 
   def execute
-    @robot.position = @position
+    @robot.position = @position unless @table.out_of_bounds? @position
   end
 end
--- spec/unit/place_command_spec.rb
+++ spec/unit/place_command_spec.rb
@@ -8,9 +8,14 @@ describe PlaceCommand do
 
       it 'should update the robots current position' do
         robot = double('Robot')
+        table = double('Table')
         position = Position.new(0, 0, :NORTH)
-        place = PlaceCommand.new robot: robot, position: position
 
+        place = PlaceCommand.new robot: robot,
+                                 position: position,
+                                 table: table
+
+        allow(table).to receive(:out_of_bounds?).with(position) { false }
         expect(robot).to receive(:position=).with(position)
 
         place.execute
@@ -21,14 +26,14 @@ describe PlaceCommand do
 
       it 'should not update the robots current position' do
         robot = double('Robot')
-        table = doublt('Table')
+        table = double('Table')
         position = Position.new(-1, -1, :NORTH)
 
         place = PlaceCommand.new robot: robot,
                                  position: position,
                                  table: table
 
-        expect(table).to receive(:out_of_bounds?).with(position)
+        expect(table).to receive(:out_of_bounds?).with(position) { true }
 
         place.execute
       end
