d35f10979dbcf1965f79b6fcb8e3aeb650d577ce
getsometoast <hijameslewis@gmail.com>
2014-11-21 15:08:21 +1100

    add failing aat for placing robot on table

--- spec/aat/place_spec.rb
+++ spec/aat/place_spec.rb
@@ -5,7 +5,8 @@ describe 'placing the robot' do
   context 'on the table' do
 
     it 'should set the robots current position' do
-      
+      input_file = 'spec/aat/data/place_on_table.txt'
+      expect { REATest::Application.new(input_file).run }.to output('0,0,NORTH\n').to_stdout
     end
   end
 end
