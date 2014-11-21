76a0ab47e52595c95601f8bb9fb0a3b67639e781
getsometoast <hijameslewis@gmail.com>
2014-11-22 10:56:44 +1100

    start to dry up the move command spec

--- spec/unit/move_command_spec.rb
+++ spec/unit/move_command_spec.rb
@@ -1,5 +1,18 @@
 require 'rspec'
 
+def it_updates_robots_position(**args)
+  robot = double('Robot')
+  table = double('Table')
+
+  allow(table).to receive(:out_of_bounds?) { false }
+  allow(robot).to receive(:position) { args[:from] }
+  expect(robot).to receive(:position=).with(args[:to])
+
+  move_command = MoveCommand.new robot: robot,
+                                 table: table
+  move_command.execute
+end
+
 describe MoveCommand do
 
   describe '#execute' do
@@ -7,16 +20,8 @@ describe MoveCommand do
     context 'heading north' do
 
       it 'updates the robots position' do
-        robot = double('Robot')
-        table = double('Table')
-
-        allow(table).to receive(:out_of_bounds?) { false }
-        allow(robot).to receive(:position) { Position.new(0, 0, :NORTH) }
-        expect(robot).to receive(:position=).with(Position.new(0, 1, :NORTH))
-
-        move_command = MoveCommand.new robot: robot,
-                                       table: table
-        move_command.execute
+        it_updates_robots_position from: Position.new(0, 0, :NORTH),
+                                   to: Position.new(0, 1, :NORTH)
       end
 
       it 'does not move the robot off the table' do
