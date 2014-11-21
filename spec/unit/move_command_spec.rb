b40e6ee49377d238727f6e2590637052eb2dd689
getsometoast <hijameslewis@gmail.com>
2014-11-21 17:54:50 +1100

    add failing test to check robot doesn't move off the table

--- spec/unit/move_command_spec.rb
+++ spec/unit/move_command_spec.rb
@@ -7,13 +7,22 @@ describe MoveCommand do
     context 'heading north' do
 
       it 'updates the robots position' do
-
         robot = double('Robot')
-
         expect(robot).to receive(:position=) { Position.new(0, 1, :NORTH) }
 
         move_command = MoveCommand.new robot: robot
+        move_command.execute
+      end
 
+      it 'does not move the robot off the table' do
+        robot = double('Robot')
+        table = double('Table')
+        current_position = Position.new(0, 5, :NORTH)
+
+        expect(robot).to receive(:position) { current_position }
+        expect(table).to receive(:out_of_bounds?).with(current_position) { true }
+
+        move_command = MoveCommand.new robot: robot
         move_command.execute
       end
     end
