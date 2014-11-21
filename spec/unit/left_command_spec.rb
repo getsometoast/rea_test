3e682c574ff6f9c2651233cf21f079410431b6a9
getsometoast <hijameslewis@gmail.com>
2014-11-22 10:51:17 +1100

    dry up left command spec

--- spec/unit/left_command_spec.rb
+++ spec/unit/left_command_spec.rb
@@ -25,42 +25,21 @@ describe LeftCommand do
     context 'currently heading west' do
 
       it 'updates robots position to head south' do
-        robot = double('Robot')
-
-        expect(robot).to receive(:position) { Position.new(0, 0, :WEST) }
-        expect(robot).to receive(:position=).with(Position.new(0, 0, :SOUTH))
-
-        left_command = LeftCommand.new robot: robot
-
-        left_command.execute
+        it_rotates_left from: :WEST, to: :SOUTH
       end
     end
 
     context 'currently heading south' do
 
       it 'updates robots position to head east' do
-        robot = double('Robot')
-
-        expect(robot).to receive(:position) { Position.new(0, 0, :SOUTH) }
-        expect(robot).to receive(:position=).with(Position.new(0, 0, :EAST))
-
-        left_command = LeftCommand.new robot: robot
-
-        left_command.execute
+        it_rotates_left from: :SOUTH, to: :EAST
       end
     end
 
     context 'currently heading east' do
 
       it 'updates robots position to head north' do
-        robot = double('Robot')
-
-        expect(robot).to receive(:position) { Position.new(0, 0, :EAST) }
-        expect(robot).to receive(:position=).with(Position.new(0, 0, :NORTH))
-
-        left_command = LeftCommand.new robot: robot
-
-        left_command.execute
+        it_rotates_left from: :EAST, to: :NORTH
       end
     end
   end
