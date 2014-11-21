3aedb5f1ee830339c1d5d42fdf707866a99c68a7
getsometoast <hijameslewis@gmail.com>
2014-11-21 16:39:35 +1100

    refactor parser place test to check #parse returns an array

--- spec/unit/input_parser_spec.rb
+++ spec/unit/input_parser_spec.rb
@@ -5,13 +5,17 @@ describe InputParser do
   describe '#parse' do
 
     it 'parses a place command' do
+      place_command = double('PlaceCommand')
       command_factory = double('CommandFactory')
 
       parser = InputParser.new command_factory: command_factory
 
-      expect(command_factory).to receive(:place).with( position: Position.new(0, 0, :NORTH) )
+      expect(command_factory).to receive(:place).with( position: Position.new(0, 0, :NORTH) ) { place_command }
 
-      parser.parse('PLACE 0,0,NORTH')
+      result = parser.parse('PLACE 0,0,NORTH')
+
+      expect(result).to be_a(Array)
+      expect(result.count).to eq(1)
     end
 
     it 'parses a report command' do
