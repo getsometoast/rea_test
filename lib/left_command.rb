2808fe83d05d63ff861a61d3ba80d8aab104b40d
getsometoast <hijameslewis@gmail.com>
2014-11-21 19:35:15 +1100

    add code to make tests pass clear

--- lib/left_command.rb
+++ lib/left_command.rb
@@ -7,6 +7,8 @@ class LeftCommand
     @robot.position = get_next_heading(@robot.position)
   end
 
+  private
+
   def get_next_heading(position)
     case position.heading
     when :NORTH then Position.new(position.x, position.y, :WEST)
--- lib/right_command.rb
+++ lib/right_command.rb
@@ -2,4 +2,19 @@ class RightCommand
   def initialize(**args)
     @robot = args[:robot]
   end
+
+  def execute
+    @robot.position = get_next_heading(@robot.position)
+  end
+
+  private
+
+  def get_next_heading(position)
+    case position.heading
+    when :NORTH then Position.new(position.x, position.y, :EAST)
+    when :EAST then Position.new(position.x, position.y, :SOUTH)
+    when :SOUTH then Position.new(position.x, position.y, :WEST)
+    when :WEST then Position.new(position.x, position.y, :NORTH)
+    end
+  end
 end
