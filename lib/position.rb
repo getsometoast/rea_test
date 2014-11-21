0f14daf7f9b127b2e59e384bc99c8c67fac3d1ef
getsometoast <hijameslewis@gmail.com>
2014-11-21 15:34:24 +1100

    add code to make position #== test pass

--- lib/position.rb
+++ lib/position.rb
@@ -1,5 +1,13 @@
 class Position
+  attr_reader :x, :y, :heading
+
   def initialize(x, y, heading)
     @x, @y, @heading = x, y, heading
+  end
+
+  def ==(other)
+    @x == other.x &&
+    @y == other.y &&
+    @heading == other.heading
   end
 end
