2f14a1e7ff60f88000e79d072e49c32520e8be44
getsometoast <hijameslewis@gmail.com>
2014-11-21 19:27:28 +1100

    add all failing aats for rotating right

---
+++ spec/aat/right_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../support/spec_helper'
+
+describe 'rotating the robot right' do
+
+  context 'heading north' do
+
+    it 'updates the robots position to heading east' do
+      input_file = 'spec/aat/data/right_from_north.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/0,0,EAST/).to_stdout
+    end
+  end
+
+  context 'heading east' do
+
+    it 'updates the robots position to heading south' do
+      input_file = 'spec/aat/data/right_from_east.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/0,0,SOUTH/).to_stdout
+    end
+  end
+
+  context 'heading south' do
+
+    it 'updates the robots position to heading west' do
+      input_file = 'spec/aat/data/right_from_south.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/0,0,WEST/).to_stdout
+    end
+  end
+
+  context 'heading west' do
+
+    it 'updates the robots position to heading north' do
+      input_file = 'spec/aat/data/right_from_west.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/0,0,NORTH/).to_stdout
+    end
+  end
+end
