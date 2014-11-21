242fe17c2acfa744b0815c5f19c1bf7c09ae7362
getsometoast <hijameslewis@gmail.com>
2014-11-21 19:01:49 +1100

    fix bad data in move south test

--- lib/table.rb
+++ lib/table.rb
@@ -1,7 +1,7 @@
 class Table
   def out_of_bounds?(position)
     return true unless position.x.between?(0,4) &&
-                        position.y.between?(0,4)
+                       position.y.between?(0,4)
 
     false
   end
