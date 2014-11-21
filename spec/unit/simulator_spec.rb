5802dd82df894914705895cf3934c525d9e1c02f
getsometoast <hijameslewis@gmail.com>
2014-11-21 16:36:43 +1100

    add test to simulator to make sure it can handle more than one command

--- spec/unit/simulator_spec.rb
+++ spec/unit/simulator_spec.rb
@@ -6,11 +6,14 @@ describe Simulator do
 
     it 'executes the commands' do
       place_command = double('PlaceCommand')
+      report_command = double('ReportCommand')
+
       simulator = Simulator.new
 
       expect(place_command).to receive(:execute)
+      expect(report_command).to receive(:execute)
 
-      simulator.run(place_command)
+      simulator.run([place_command, report_command])
     end
   end
 end
