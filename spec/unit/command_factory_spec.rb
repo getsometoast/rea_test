92d8135656a13a88e85a9ee29edbc125e0e4fbbd
getsometoast <hijameslewis@gmail.com>
2014-11-21 16:00:30 +1100

    add failing rest for command factory#report

--- spec/unit/command_factory_spec.rb
+++ spec/unit/command_factory_spec.rb
@@ -11,4 +11,14 @@ describe CommandFactory do
       expect(command).to be_a(PlaceCommand)
     end
   end
+
+  describe '#report' do
+
+    it 'creates a report command' do
+      command_factory = CommandFactory.new
+      command = command_factory.report
+
+      expect(command).to be_a(ReportCommand)
+    end
+  end
 end
