53fffb8a8f2357faa8048b0b0f6d2f326525bbe2
getsometoast <hijameslewis@gmail.com>
2014-11-21 15:06:41 +1100

    intial commit

---
+++ Rakefile
@@ -0,0 +1,7 @@
+require 'rspec/core/rake_task'
+
+task :spec do
+  RSpec::Core::RakeTask.new(:spec)
+end
+
+task :default => :spec
---
+++ spec/aat/place_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+
+describe 'placing the robot' do
+
+  context 'on the table' do
+
+    it 'should set the robots current position' do
+      
+    end
+  end
+end
