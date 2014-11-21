4ae5560a0dacb4a97d7b5966668cee45dc788890
getsometoast <hijameslewis@gmail.com>
2014-11-21 18:05:08 +1100

    add failing tests for move command

--- spec/unit/move_command_spec.rb
+++ spec/unit/move_command_spec.rb
@@ -33,5 +33,35 @@ describe MoveCommand do
         move_command.execute
       end
     end
+
+    context 'heading south' do
+
+      it 'updates the robots position' do
+        robot = double('Robot')
+        table = double('Table')
+
+        allow(table).to receive(:out_of_bounds?) { false }
+        allow(robot).to receive(:position) { Position.new(0, 1, :SOUTH) }
+        expect(robot).to receive(:position=) { Position.new(0, 0, :SOUTH) }
+
+        move_command = MoveCommand.new robot: robot,
+                                       table: table
+        move_command.execute
+      end
+
+      it 'does not move the robot off the table' do
+        robot = double('Robot')
+        table = double('Table')
+        current_position = Position.new(0, 0, :SOUTH)
+        next_position = Position.new(0, -1, :SOUTH)
+
+        expect(robot).to receive(:position) { current_position }
+        expect(table).to receive(:out_of_bounds?).with(next_position) { true }
+
+        move_command = MoveCommand.new robot: robot,
+                                       table: table
+        move_command.execute
+      end
+    end
   end
 end
