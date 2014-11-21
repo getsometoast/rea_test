3a231aa089aee06b542961f5137b2dbf80647df3
getsometoast <hijameslewis@gmail.com>
2014-11-21 19:17:51 +1100

    make test pass

--- lib/left_command.rb
+++ lib/left_command.rb
@@ -4,6 +4,12 @@ class LeftCommand
   end
 
   def execute
-    @robot.position = Position.new(0, 0, :WEST)
+    @robot.position = get_next_heading(@robot.position)
+  end
+
+  def get_next_heading(position)
+    case position.heading
+    when :NORTH then Position.new(position.x, position.y, :WEST)
+    end
   end
 end
