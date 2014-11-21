c34322e27e9c0f7d8b56f0a29cd64c96b605c955
getsometoast <hijameslewis@gmail.com>
2014-11-21 16:28:36 +1100

    add test for table out_of_bounds? on table

--- spec/unit/table_spec.rb
+++ spec/unit/table_spec.rb
@@ -5,8 +5,11 @@ describe Table do
   describe '#out_of_bounds?' do
 
     it 'returns true if a position is off table' do
-
       expect(Table.new.out_of_bounds? Position.new(-1, -1, :NORTH)).to eq(true)
+    end
+
+    it 'returns false if a position is on table' do
+      expect(Table.new.out_of_bounds? Position.new(0, 0, :NORTH)).to eq(false)
     end
   end
 end
