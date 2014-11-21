42c7469308ef719f47593293f11e8ee056cd9cee
getsometoast <hijameslewis@gmail.com>
2014-11-21 17:48:30 +1100

    add failing test for move command #execute

---
+++ spec/unit/move_command_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+
+describe MoveCommand do
+
+  describe '#execute' do
+
+    context 'heading north' do
+
+      it 'updates the robots position' do
+
+        robot = double('Robot')
+
+        expect(robot).to receive(:position) { Position.new(0, 0, :NORTH) }
+
+        move_command = MoveCommand.new robot: robot
+
+        move_command.execute
+      end
+    end
+  end
+end
