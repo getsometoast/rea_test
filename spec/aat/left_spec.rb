e40402119bdd3d804084874214693d55b9035e0c
getsometoast <hijameslewis@gmail.com>
2014-11-21 19:23:34 +1100

    add failing aats for other left rotations

--- spec/aat/left_spec.rb
+++ spec/aat/left_spec.rb
@@ -17,4 +17,20 @@ describe 'rotating the robot left' do
       expect { REATest::Application.new(input_file).run }.to output(/0,0,SOUTH/).to_stdout
     end
   end
+
+  context 'heading south' do
+
+    it 'updates the robots position to heading east' do
+      input_file = 'spec/aat/data/left_from_south.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/0,0,EAST/).to_stdout
+    end
+  end
+
+  context 'heading east' do
+
+    it 'updates the robots position to heading north' do
+      input_file = 'spec/aat/data/left_from_east.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/0,0,NORTH/).to_stdout
+    end
+  end
 end
