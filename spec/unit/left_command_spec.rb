eb451e90682953c21d6ebaf2f6c604c46b11c67e
getsometoast <hijameslewis@gmail.com>
2014-11-21 19:13:30 +1100

    add failing test for left command turning from north

---
+++ spec/unit/left_command_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../support/spec_helper'
+
+describe LeftCommand do
+
+  describe '#execute' do
+
+    context 'currently heading north' do
+
+      it 'updates robots position to head west' do
+        robot = double('Robot')
+
+        expect(robot).to receive(:position=).with(Position.new(0, 0, :WEST))
+
+        left_command = LeftCommand.new robot: robot
+
+        left_command.execute
+      end
+    end
+  end
+end
