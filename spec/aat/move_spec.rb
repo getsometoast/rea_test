7b453969b5e3947ad9ae77802298d20a423b9717
getsometoast <hijameslewis@gmail.com>
2014-11-21 17:55:51 +1100

    meant to add that as an aat..

--- spec/aat/move_spec.rb
+++ spec/aat/move_spec.rb
@@ -8,5 +8,10 @@ describe 'moving the robot' do
       input_file = 'spec/aat/data/move_north.txt'
       expect { REATest::Application.new(input_file).run }.to output(/0,1,NORTH/).to_stdout
     end
+
+    it 'does not move off the table' do
+      input_file = 'spec/aat/data/move_north_off.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/0,4,NORTH/).to_stdout
+    end
   end
 end
