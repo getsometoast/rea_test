871a1a354ec469dac66fd93e721e66c80f902daf
getsometoast <hijameslewis@gmail.com>
2014-11-22 11:06:46 +1100

    dry up move command with helper methods

--- spec/support/move_command_helpers.rb
+++ spec/support/move_command_helpers.rb
@@ -1,11 +1,25 @@
 module MoveCommandHelpers
-  def it_updates_robots_position(**args)
+  def it_moves(**args)
     robot = double('Robot')
     table = double('Table')
 
     allow(table).to receive(:out_of_bounds?) { false }
     allow(robot).to receive(:position) { args[:from] }
     expect(robot).to receive(:position=).with(args[:to])
+
+    move_command = MoveCommand.new robot: robot,
+                                   table: table
+    move_command.execute
+  end
+
+  def it_does_not_move(**args)
+    robot = double('Robot')
+    table = double('Table')
+    current_position = args[:from]
+    next_position = args[:to]
+
+    expect(robot).to receive(:position) { current_position }
+    expect(table).to receive(:out_of_bounds?).with(next_position) { true }
 
     move_command = MoveCommand.new robot: robot,
                                    table: table
--- spec/unit/move_command_spec.rb
+++ spec/unit/move_command_spec.rb
@@ -7,88 +7,52 @@ describe MoveCommand do
     context 'heading north' do
 
       it 'updates the robots position' do
-        it_updates_robots_position from: Position.new(0, 0, :NORTH),
-                                   to: Position.new(0, 1, :NORTH)
+        it_moves from: Position.new(0, 0, :NORTH),
+                 to: Position.new(0, 1, :NORTH)
       end
 
       it 'does not move the robot off the table' do
-        robot = double('Robot')
-        table = double('Table')
-        current_position = Position.new(0, 4, :NORTH)
-        next_position = Position.new(0, 5, :NORTH)
-
-        expect(robot).to receive(:position) { current_position }
-        expect(table).to receive(:out_of_bounds?).with(next_position) { true }
-
-        move_command = MoveCommand.new robot: robot,
-                                       table: table
-        move_command.execute
+        it_does_not_move from: Position.new(0, 4, :NORTH),
+                         to: Position.new(0, 5, :NORTH)
       end
     end
 
     context 'heading south' do
 
       it 'updates the robots position' do
-        it_updates_robots_position from: Position.new(0, 1, :SOUTH),
-                                   to: Position.new(0, 0, :SOUTH)
+        it_moves from: Position.new(0, 1, :SOUTH),
+                 to: Position.new(0, 0, :SOUTH)
       end
 
       it 'does not move the robot off the table' do
-        robot = double('Robot')
-        table = double('Table')
-        current_position = Position.new(0, 0, :SOUTH)
-        next_position = Position.new(0, -1, :SOUTH)
-
-        expect(robot).to receive(:position) { current_position }
-        expect(table).to receive(:out_of_bounds?).with(next_position) { true }
-
-        move_command = MoveCommand.new robot: robot,
-                                       table: table
-        move_command.execute
+        it_does_not_move from: Position.new(0, 0, :SOUTH),
+                         to: Position.new(0, -1, :SOUTH)
       end
     end
 
     context 'heading east' do
 
       it 'updates the robots position' do
-        it_updates_robots_position from: Position.new(0, 0, :EAST),
-                                   to: Position.new(1, 0, :EAST)
+        it_moves from: Position.new(0, 0, :EAST),
+                 to: Position.new(1, 0, :EAST)
       end
 
       it 'does not move the robot off the table' do
-        robot = double('Robot')
-        table = double('Table')
-        current_position = Position.new(4, 0, :EAST)
-        next_position = Position.new(5, 0, :EAST)
-
-        expect(robot).to receive(:position) { current_position }
-        expect(table).to receive(:out_of_bounds?).with(next_position) { true }
-
-        move_command = MoveCommand.new robot: robot,
-                                       table: table
-        move_command.execute
+        it_does_not_move from: Position.new(4, 0, :EAST),
+                         to: Position.new(5, 0, :EAST)
       end
     end
 
     context 'heading west' do
 
       it 'updates the robots position' do
-        it_updates_robots_position from: Position.new(1, 0, :WEST),
-                                   to: Position.new(0, 0, :WEST)
+        it_moves from: Position.new(1, 0, :WEST),
+                 to: Position.new(0, 0, :WEST)
       end
 
       it 'does not move the robot off the table' do
-        robot = double('Robot')
-        table = double('Table')
-        current_position = Position.new(0, 0, :WEST)
-        next_position = Position.new(-1, 0, :WEST)
-
-        expect(robot).to receive(:position) { current_position }
-        expect(table).to receive(:out_of_bounds?).with(next_position) { true }
-
-        move_command = MoveCommand.new robot: robot,
-                                       table: table
-        move_command.execute
+        it_does_not_move from: Position.new(0, 0, :WEST),
+                         to: Position.new(-1, 0, :WEST)
       end
     end
   end
