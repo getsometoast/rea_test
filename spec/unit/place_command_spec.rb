05447dc987ca0a515fbadaf4c570cfe6afa7c4ea
getsometoast <hijameslewis@gmail.com>
2014-11-22 11:45:54 +1100

    dry up the place command spec

--- spec/unit/place_command_spec.rb
+++ spec/unit/place_command_spec.rb
@@ -2,21 +2,24 @@ require_relative '../support/spec_helper'
 
 describe PlaceCommand do
 
+  before :each do
+    @robot = double('Robot')
+    @table = double('Table')
+  end
+
   describe '#execute' do
 
     context 'position within table bounds' do
 
       it 'should update the robots current position' do
-        robot = double('Robot')
-        table = double('Table')
         position = Position.new(0, 0, :NORTH)
 
-        place = PlaceCommand.new robot: robot,
+        place = PlaceCommand.new robot: @robot,
                                  position: position,
-                                 table: table
+                                 table: @table
 
-        allow(table).to receive(:out_of_bounds?).with(position) { false }
-        expect(robot).to receive(:position=).with(position)
+        allow(@table).to receive(:out_of_bounds?).with(position) { false }
+        expect(@robot).to receive(:position=).with(position)
 
         place.execute
       end
@@ -25,17 +28,15 @@ describe PlaceCommand do
     context 'position is outside table bounds' do
 
       it 'should not update the robots current position' do
-        robot = double('Robot')
-        table = double('Table')
         position = Position.new(-1, -1, :NORTH)
 
-        place = PlaceCommand.new robot: robot,
+        place = PlaceCommand.new robot: @robot,
                                  position: position,
-                                 table: table
+                                 table: @table
 
-        expect(table).to receive(:out_of_bounds?).with(position) { true }
-        expect(robot).to_not receive(:position=).with(position)
-        
+        expect(@table).to receive(:out_of_bounds?).with(position) { true }
+        expect(@robot).to_not receive(:position=).with(position)
+
         place.execute
       end
     end
