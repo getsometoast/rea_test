70cf53fe84ba9bc89e0fc71869ab467af970f361
getsometoast <hijameslewis@gmail.com>
2014-11-24 09:33:02 +1100

    add more failing tests for parsing badly formed commands

--- spec/aat/bad_data_spec.rb
+++ spec/aat/bad_data_spec.rb
@@ -9,4 +9,44 @@ describe 'parsing commands' do
       expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
     end
   end
+
+  context 'when right command is badly formed' do
+
+    it 'should ignore the command' do
+      input_file = 'spec/aat/data/bad_data/right_command.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+    end
+  end
+
+  context 'when move command is badly formed' do
+
+    it 'should ignore the command' do
+      input_file = 'spec/aat/data/bad_data/move_command.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+    end
+  end
+
+  context 'when place command is badly formed' do
+
+    it 'should ignore the command' do
+      input_file = 'spec/aat/data/bad_data/place_command.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+    end
+  end
+
+  context 'when report command is badly formed' do
+
+    it 'should ignore the command' do
+      input_file = 'spec/aat/data/bad_data/report_command.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+    end
+  end
+
+  context 'when place position is badly formed' do
+
+    it 'should ignore the command' do
+      input_file = 'spec/aat/data/bad_data/place_position.txt'
+      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+    end
+  end
 end
