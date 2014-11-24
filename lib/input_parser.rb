b5eb5a875544f9c0c483820bdd0f6c9e53abd66b
getsometoast <hijameslewis@gmail.com>
2014-11-24 11:59:52 +1100

    refactor parser to be more readable

--- lib/input_parser.rb
+++ lib/input_parser.rb
@@ -4,35 +4,48 @@ class InputParser
   end
 
   def parse(text)
-    get_commands_from(text)
+    get_lines_from(text).map { |line| get_command_from(line) }
   end
 
   private
 
-  def get_commands_from(text)
-    text.split(/\n/).map { |line| get_command_from(line) }
+  def get_lines_from(text)
+    text.split(/\n/)
   end
 
   def get_command_from(line)
     fail Errors::BadlyFormedCommand unless can_parse? line
 
-    type = line.split(/\s/)[0]
+    type = get_type_from(line)
 
-    case type.to_sym
-    when :PLACE then @command_factory.place position: get_position_from_line(line)
-    when :REPORT then @command_factory.report
-    when :MOVE then @command_factory.move
-    when :LEFT then @command_factory.left
-    when :RIGHT then @command_factory.right
-    end
-  end
-
-  def get_position_from_line(line)
-    tokens = line.split(/\s/)[1].split(',')
-    Position.new(tokens[0].to_i, tokens[1].to_i, tokens[2].to_sym)
+    create_command type: type, from: line
   end
 
   def can_parse?(line)
     line =~ /(MOVE|LEFT|RIGHT|REPORT)|(^PLACE\s\d,\d,(NORTH|SOUTH|EAST|WEST)$)/
+  end
+
+  def get_type_from(line)
+    line.split(/\s/)[0].to_sym
+  end
+
+  def create_command(**args)
+    case args[:type]
+    when :PLACE then
+      @command_factory.place position: get_position_from(args[:from])
+    when :REPORT then
+      @command_factory.report
+    when :MOVE then
+      @command_factory.move
+    when :LEFT then
+      @command_factory.left
+    when :RIGHT then
+      @command_factory.right
+    end
+  end
+
+  def get_position_from(line)
+    tokens = line.split(/\s/)[1].split(',')
+    Position.new(tokens[0].to_i, tokens[1].to_i, tokens[2].to_sym)
   end
 end
