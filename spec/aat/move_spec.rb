29fc5175b0ca28ac3a4bb56a4db50e3157240851
getsometoast <hijameslewis@gmail.com>
2014-11-24 12:30:45 +1100

    dry up move aats

--- spec/aat/move_spec.rb
+++ spec/aat/move_spec.rb
@@ -5,52 +5,44 @@ describe 'moving the robot' do
   context 'when facing north' do
 
     it 'moves the robot forward north' do
-      input_file = 'spec/aat/data/move/move_north.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/0,1,NORTH/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/move/move_north.txt', output: /0,1,NORTH/
     end
 
     it 'does not move off the table' do
-      input_file = 'spec/aat/data/move/move_north_off.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/0,4,NORTH/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/move/move_north_off.txt', output: /0,4,NORTH/
     end
   end
 
   context 'when facing south' do
 
     it 'moves the robot forward south' do
-      input_file = 'spec/aat/data/move/move_south.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/0,0,SOUTH/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/move/move_south.txt', output: /0,0,SOUTH/
     end
 
     it 'does not move off the table' do
-      input_file = 'spec/aat/data/move/move_south_off.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/0,0,SOUTH/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/move/move_south_off.txt', output: /0,0,SOUTH/
     end
   end
 
   context 'when facing east' do
 
     it 'moves the robot forward east' do
-      input_file = 'spec/aat/data/move/move_east.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/1,0,EAST/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/move/move_east.txt', output: /1,0,EAST/
     end
 
     it 'does not move off the table' do
-      input_file = 'spec/aat/data/move/move_east_off.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/4,0,EAST/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/move/move_east_off.txt', output: /4,0,EAST/
     end
   end
 
   context 'when facing west' do
 
     it 'moves the robot forward west' do
-      input_file = 'spec/aat/data/move/move_west.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/0,0,WEST/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/move/move_west.txt', output: /0,0,WEST/
     end
 
     it 'does not move off the table' do
-      input_file = 'spec/aat/data/move/move_west_off.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/0,0,WEST/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/move/move_west_off.txt', output: /0,0,WEST/
     end
   end
 end
