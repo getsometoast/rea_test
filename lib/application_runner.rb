ab4ee257da031b9772b33ad9c9c335e08425975a
getsometoast <hijameslewis@gmail.com>
2014-11-24 12:13:52 +1100

    add application runner and test data

--- lib/application_runner.rb
+++ lib/application_runner.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
 
+require_relative 'application'
+require_relative 'table'
+require_relative 'Robot'
+require_relative 'commands/factory'
+require_relative 'commands/move'
+require_relative 'commands/place'
+require_relative 'commands/left'
+require_relative 'commands/right'
+require_relative 'commands/report'
+require_relative 'input_parser'
+require_relative 'badly_formed_command'
+require_relative 'position'
+require_relative 'simulator'
+
 REATest::Application.new(ARGV[0]).run
