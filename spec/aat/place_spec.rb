51fd594135d15e7c583e1a2eeff5c670c6b277b7
getsometoast <hijameslewis@gmail.com>
2014-11-24 12:31:50 +1100

    dry up place aats

--- spec/aat/place_spec.rb
+++ spec/aat/place_spec.rb
@@ -5,16 +5,14 @@ describe 'placing the robot' do
   context 'on the table' do
 
     it 'should set the robots current position' do
-      input_file = 'spec/aat/data/place/place_on_table.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/0,0,NORTH/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/place/place_on_table.txt', output: /0,0,NORTH/
     end
   end
 
   context 'off the table' do
 
     it 'should ignore the command' do
-      input_file = 'spec/aat/data/place/place_off_table.txt'
-      expect { REATest::Application.new(input_file).run }.to output('').to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/place/place_off_table.txt', output: ''
     end
   end
 end
