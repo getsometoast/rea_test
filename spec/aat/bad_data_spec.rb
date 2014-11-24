4a921ef1c0cf25646932765ff6f77dbb11c6fb55
getsometoast <hijameslewis@gmail.com>
2014-11-24 12:36:40 +1100

    dry up badly formed command aats

--- spec/aat/bad_data_spec.rb
+++ spec/aat/bad_data_spec.rb
@@ -5,48 +5,48 @@ describe 'parsing commands' do
   context 'when left command is badly formed' do
 
     it 'should ignore the command' do
-      input_file = 'spec/aat/data/bad_data/left_command.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/bad_data/left_command.txt',
+                                      output: /Badly formed command in input file/
     end
   end
 
   context 'when right command is badly formed' do
 
     it 'should ignore the command' do
-      input_file = 'spec/aat/data/bad_data/right_command.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/bad_data/right_command.txt',
+                                      output: /Badly formed command in input file/
     end
   end
 
   context 'when move command is badly formed' do
 
     it 'should ignore the command' do
-      input_file = 'spec/aat/data/bad_data/move_command.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/bad_data/move_command.txt',
+                                      output: /Badly formed command in input file/
     end
   end
 
   context 'when place command is badly formed' do
 
     it 'should ignore the command' do
-      input_file = 'spec/aat/data/bad_data/place_command.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/bad_data/place_command.txt',
+                                      output: /Badly formed command in input file/
     end
   end
 
   context 'when report command is badly formed' do
 
     it 'should ignore the command' do
-      input_file = 'spec/aat/data/bad_data/report_command.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/bad_data/report_command.txt',
+                                      output: /Badly formed command in input file/
     end
   end
 
   context 'when place position is badly formed' do
 
     it 'should ignore the command' do
-      input_file = 'spec/aat/data/bad_data/place_position.txt'
-      expect { REATest::Application.new(input_file).run }.to output(/Badly formed command in input file/).to_stdout
+      system_produces_expected_output input_file: 'spec/aat/data/bad_data/place_position.txt',
+                                      output: /Badly formed command in input file/
     end
   end
 end
